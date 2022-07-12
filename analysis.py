e2b5e1ffbe9c39e73c0f6e376f23a13e215702bf
Ary Neto <aryneto@poli.ufrj.br>
2022-07-12 11:31:44 -0300

    teste 07/07

--- analysis.py
+++ analysis.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 INTERVAL_STEP = 100
 
+def cluster_type(value):
+    for i in range(INTERVAL_STEP):
+        if value < (i + 1) * step:
+            return i
+
 
 df_brute_test_12_all = pd.read_csv("tests/brute_test_5regular12-all.g6.csv")
 
@@ -34,6 +39,8 @@ step_brute = (max_brute - min_brute) / INTERVAL_STEP
 step_direct = (max_direto - min_direto) / INTERVAL_STEP
 step_angle = (max_angulo - min_angulo) / INTERVAL_STEP
 
+step = step_brute
+
 print(f'\n\nMAX: \nBrute: {max_brute}\nDireto: {max_direto}\nAngulo: {max_angulo}')
 
 print(f'\n\nMIN: \nBrute: {min_brute}\nDireto: {min_direto}\nAngulo: {min_angulo}')
@@ -41,4 +48,6 @@ print(f'\n\nMIN: \nBrute: {min_brute}\nDireto: {min_direto}\nAngulo: {min_angulo
 print(f'\n\nMEAN: \nBrute: {mean_brute}\nDireto: {mean_direto}\nAngulo: {mean_angulo}')
 
 print(f'\n\nSTEP: \nBrute: {step_brute}\nDireto: {step_direct}\nAngulo: {step_angle}')
+
+df_brute_test_12_all['cluster'] = df_brute_test_12_all['tempo_total'].apply()
 
